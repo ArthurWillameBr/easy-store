49669962b5a66c77ae54edfd0e27a2e5b31c3c99
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-04-27 18:40:21 -0300

    feat: add first discount banner to home page

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-5">
+      <Image src="/bannerDescont.png" width={0} height={0} className="h-auto w-full " sizes="100vw" alt="até 55% de desconto esse mês" />
+    </div>
+  );
+}
