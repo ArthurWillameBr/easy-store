a400c2332eafaee2f0fe529c7ccd45ea13e65e38
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-04-29 15:12:09 -0300

    feat: ensure user goes  to home page on catalog button click

--- src/components/ui/header.tsx
+++ src/components/ui/header.tsx
@@ -108,10 +108,12 @@ export function Header() {
           </div>
         </SheetContent>
       </Sheet>
-      <h1 className="text-lg font-semibold ">
-        {" "}
-        <span className="text-primary">Easy </span>Store
-      </h1>
+      <Link href="/">
+        <h1 className="text-lg font-semibold ">
+          {" "}
+          <span className="text-primary">Easy </span>Store
+        </h1>
+      </Link>
       <Button size="icon" variant="outline">
         <ShoppingCart />
       </Button>
