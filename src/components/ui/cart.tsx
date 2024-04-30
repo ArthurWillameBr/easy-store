f4f30faaeca5d1f78d4871d5b3536f0428212da4
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-04-30 13:47:44 -0300

    feat: ensure cart products can e scrolled

--- src/components/ui/cart.tsx
+++ src/components/ui/cart.tsx
@@ -5,11 +5,12 @@ import { CartContext } from "@/providers/cart";
 import { CartItem } from "./cart-item";
 import { computeProductTotalPrice } from "@/helpers/product";
 import { Separator } from "./separator";
-
+import { ScrollArea } from "./scroll-area";
+import { Button } from "./button";
 export const Cart = () => {
   const { products, subtotal, total, totalDiscount } = useContext(CartContext);
   return (
-    <div className="flex flex-col gap-8">
+    <div className="flex h-full flex-col gap-8">
       <Badge
         className="w-fit gap-1 border-2 border-primary px-3 py-[0.375rem] text-base uppercase"
         variant="outline"
@@ -18,40 +19,47 @@ export const Cart = () => {
         Catálogo
       </Badge>
 
-      <div className="flex flex-col gap-5">
-        {products.length > 0 ? (
-            products.map((product) => (
-              <CartItem
-                key={product.id}
-                product={computeProductTotalPrice(product as any) as any}
-              />
-            ))
-        ) : (
-          <p className="text-center font-semibold">Carrinho vazio. Vamos fazer compras?</p>
-        ) }
+      <div className="flex h-full flex-col gap-5">
+        <ScrollArea className="max-h-[40rem]">
+          <div className="flex flex-col gap-5">
+            {products.length > 0 ? (
+              products.map((product) => (
+                <CartItem
+                  key={product.id}
+                  product={computeProductTotalPrice(product as any) as any}
+                />
+              ))
+            ) : (
+              <p className="text-center font-semibold">
+                Carrinho vazio. Vamos fazer compras?
+              </p>
+            )}
+          </div>
+        </ScrollArea>
       </div>
 
       <div className="flex flex-col gap-3">
-        <Separator/>
-        <div className="flex justify-between items-center text-xs ">
+        <Separator />
+        <div className="flex items-center justify-between text-xs ">
           <p>Subtotal</p>
           <p>R$ {subtotal.toFixed(2)}</p>
         </div>
-        <Separator/>
-        <div className="flex justify-between items-center text-xs ">
+        <Separator />
+        <div className="flex items-center justify-between text-xs ">
           <p>Entrega</p>
           <p>GRÁTIS</p>
         </div>
-        <Separator/>
-        <div className="flex justify-between items-center text-xs ">
+        <Separator />
+        <div className="flex items-center justify-between text-xs ">
           <p>Descontos</p>
           <p>- R$ {totalDiscount.toFixed(2)}</p>
         </div>
-        <Separator/>
-        <div className="flex justify-between items-center text-sm font-bold">
+        <Separator />
+        <div className="flex items-center justify-between text-sm font-bold">
           <p>Total</p>
           <p>R$ {total.toFixed(2)}</p>
         </div>
+        <Button className="uppercase font-bold mt-7">Finalizar compra</Button>
       </div>
     </div>
   );
