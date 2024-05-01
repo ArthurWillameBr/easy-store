e470a8d698f36f141f6b46df73bfd7e4843c38f2
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-01 11:47:43 -0300

    fix: change skeleton loading

--- src/app/product/loading.tsx
+++ src/app/product/loading.tsx
@@ -1,30 +1,25 @@
 import { Skeleton } from "@/components/ui/skeleton";
 
 const Loading = () => {
-    return (
-        <div className="flex flex-col p-5">
-        <Skeleton className="flex h-[380px] w-full items-center justify-center" />
-        <div className="mt-8 grid grid-cols-4 gap-4 px-5">
-          <Skeleton className="flex h-[100px] items-center justify-center rounded-lg" />
-          <Skeleton className="flex h-[100px] items-center justify-center rounded-lg" />
-          <Skeleton className="flex h-[100px] items-center justify-center rounded-lg" />
-          <Skeleton className="flex h-[100px] items-center justify-center rounded-lg" />
-        </div>
-        <div className="flex flex-col mt-5 gap-1">
-            <Skeleton className="flex w-[200px] h-[24px] items-center justify-center rounded-lg"/>
-            <Skeleton className="flex w-[88px] h-[16px] items-center justify-center rounded-lg"/>
-            <Skeleton className="flex w-[70px] h-[10px] items-center justify-center rounded-lg"/>
-        </div>
-
-        <div className="flex gap-2 my-4">
-            <Skeleton className="flex w-[40px] h-[40px] items-center justify-center rounded-lg"/>
-            <div className="flex items-center justify-center">
-                <Skeleton className="flex w-[8px] h-[22px] items-center justify-center rounded-lg"/>
-            </div>
-            <Skeleton className="flex w-[40px] h-[40px] items-center justify-center rounded-lg"/>
-        </div>
+  return (
+    <div className="flex flex-col p-5">
+      <Skeleton className="flex h-[380px] w-full items-center justify-center" />
+      <div className="mt-8 grid grid-cols-4 gap-4 px-5">
+        <Skeleton className="flex h-[100px] items-center justify-center rounded-lg" />
+        <Skeleton className="flex h-[100px] items-center justify-center rounded-lg" />
+        <Skeleton className="flex h-[100px] items-center justify-center rounded-lg" />
+        <Skeleton className="flex h-[100px] items-center justify-center rounded-lg" />
       </div>
-    )
-}
+      <div className="mt-5 flex flex-col gap-1">
+        <Skeleton className="flex h-[24px] w-[200px] items-center justify-center rounded-lg" />
+        <Skeleton className="flex h-[16px] w-[88px] items-center justify-center rounded-lg" />
+        <Skeleton className="flex h-[10px] w-[70px] items-center justify-center rounded-lg" />
+      </div>
+      <div>
+        <Skeleton className="mt-5 flex h-[30px] w-[100px] rounded-md" />
+      </div>
+    </div>
+  );
+};
 
 export default Loading;
