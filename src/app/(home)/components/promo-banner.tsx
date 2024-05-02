a964af5da4d3008fac903a874b06f86920e7a770
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-02 09:53:44 -0300

    feat: add desktop banner to home page

--- src/app/(home)/components/promo-banner.tsx
+++ src/app/(home)/components/promo-banner.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image"
+import {cn} from "@/lib/utils"
 import { ImageProps } from "next/image"
-export const PromoBanner = ({alt, ...props}: ImageProps) => {
+export const PromoBanner = ({alt, className, ...props}: ImageProps) => {
     return(
         <Image
         width={0}
         height={0}
-        className="h-auto w-full px-5"
+        className={cn("h-auto w-full px-5", className)}
         sizes="100vw"
         alt={alt}
         {...props}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,6 +3,7 @@ import { prismaClient } from "@/lib/prisma";
 import { ProductList } from "../../components/ui/product-list";
 import { SectionTitle } from "../../components/ui/section-title";
 import { PromoBanner } from "./components/promo-banner";
+import Image from "next/image";
 
 export default async function Home() {
   const deals = await prismaClient.product.findMany({
@@ -16,26 +17,38 @@ export default async function Home() {
   const keyboards = await prismaClient.product.findMany({
     where: {
       category: {
-        slug: "keyboards"
-      }
-    }
-  })
+        slug: "keyboards",
+      },
+    },
+  });
 
   const mouses = await prismaClient.product.findMany({
     where: {
       category: {
-        slug: "mouses"
-      }
-    }
-  
-  })
+        slug: "mouses",
+      },
+    },
+  });
 
   return (
-    <div className="flex flex-col gap-8 py-8">
+    <>
+      <div className="max-w-[1920px] mx-auto">
+      <Image
+        src="/bannerDiscountDesktop.png"
+        alt="até 55% de desconto esse mês"
+        className="hidden h-auto w-full lg:block"
+        width={0}
+        height={0}
+        sizes="100vw"
+      />
+      <div className="container mx-auto flex flex-col gap-8 py-8">
       <PromoBanner
         src="/bannerDiscount.png"
         alt="até 55% de desconto esse mês"
+        className="lg:hidden"
       />
+     </div>
+     
       <div className="px-5">
         <Categories />
       </div>
@@ -54,14 +67,12 @@ export default async function Home() {
         <ProductList products={keyboards} />
       </div>
 
-      <PromoBanner
-        src="/bannerFones.png"
-        alt="até 20% de desconto em fones"
-      />
+      <PromoBanner src="/bannerFones.png" alt="até 20% de desconto em fones" />
       <div>
         <SectionTitle>Mouses</SectionTitle>
         <ProductList products={mouses} />
       </div>
-    </div>
+      </div>
+    </>
   );
 }
