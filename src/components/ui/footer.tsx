88e27abee100f837990e676f38b83e92af4cd214
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-02 11:10:36 -0300

    fix: footer styler

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,3 +1,9 @@
 export const Footer = () => {
-  return <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 lg:text-sm lg:pl-48 lg:py-5">₢ 2024 Copyright <span className="font-semibold">Easy Store</span></div>;
+  return (
+    <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 lg:py-5 lg:pl-48 lg:text-sm">
+      <div className="mx-auto lg:container">
+        ₢ 2024 Copyright <span className="font-semibold">Easy Store</span>
+      </div>
+    </div>
+  );
 };
