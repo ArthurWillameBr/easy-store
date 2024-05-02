50f10f96e1a7c809b748741b083ef793c39671b4
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-02 11:09:42 -0300

    feat: ensure footer is responsive

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,3 +1,3 @@
 export const Footer = () => {
-  return <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75">₢ 2024 Copyright <span className="font-semibold">Easy Store</span></div>;
+  return <div className="bg-accent px-8 py-4 text-[0.625rem] opacity-75 lg:text-sm lg:pl-48 lg:py-5">₢ 2024 Copyright <span className="font-semibold">Easy Store</span></div>;
 };
