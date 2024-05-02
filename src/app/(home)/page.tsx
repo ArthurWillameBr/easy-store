07dfb1104062292ff5397cb5b898504e26c513c8
arthurwillame5 <135058422+arthurwillame5@users.noreply.github.com>
2024-05-02 10:17:29 -0300

    feat: ensure categories are responsive for desktop

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -41,27 +41,29 @@ export default async function Home() {
         height={0}
         sizes="100vw"
       />
-      <div className="container mx-auto flex flex-col gap-8 py-8">
+</div>
+      <div className="mx-auto flex flex-col gap-8 py-8 lg:container lg:gap-10">
       <PromoBanner
         src="/bannerDiscount.png"
         alt="até 55% de desconto esse mês"
         className="lg:hidden"
       />
-     </div>
      
-      <div className="px-5">
+     
+      <div className="px-5 lg:mt-2">
         <Categories />
       </div>
 
-      <div>
+      <div className="flex flex-col gap-3 lg:gap-1">
         <SectionTitle>Ofertas</SectionTitle>
         <ProductList products={deals} />
       </div>
-
+    <div className="flex flex-col lg:flex-row">
       <PromoBanner
         src="/bannerMouses.png"
         alt="até 55% de desconto em mouses"
       />
+      </div>
       <div>
         <SectionTitle>Teclados</SectionTitle>
         <ProductList products={keyboards} />
